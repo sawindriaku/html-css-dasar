b5d65380cc279a36f8b09bb650ffeb3ff2b0d7c0
unknown <indrisawindri2@gmail.com>
2025-08-17 02:09:43 +0700

    javascript end

--- javascript/script.js
+++ javascript/script.js
@@ -35,3 +35,38 @@ switch (warnaLampu) {
     console.log("lampu padam");
     break;
 }
+
+// function
+function mobil(mobil1) {
+  mobil1 = "kijang";
+  document.getElementById("mobil").innerHTML =
+    "ini pemanggilan function menggunakan nama mobil " + mobil1;
+}
+// pemanggilan nama function di atas
+mobil();
+
+// fungsi ini tidak di eksekusi karena parameter mobil1 tidak terdefinisi, karena tidak di dalam function
+// document.getElementById("mobil").innerHTML =
+//   "ini pemanggilan function di luar " + mobil1;
+
+//   inisialisasi method
+var namaMobil = {
+  nama: "Toyota",
+  type: "Agya",
+  warna: "merah",
+  maju: function () {
+    return "mulai melaju";
+  },
+  mundur: function () {
+    return "mulai mundur";
+  },
+};
+console.log(
+  namaMobil.nama +
+    " " +
+    namaMobil.warna +
+    " " +
+    namaMobil.maju() +
+    " dan " +
+    namaMobil.mundur()
+);
